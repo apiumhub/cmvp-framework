b1d9a406dcac7de57faff96bcd15f76bf83fbda6
José manuel Barroso Galindo <theypsilon@gmail.com>
2015-04-23 14:30:16 +0700

    Fixing tests

--- src/cmvp/views/BaseView.js
+++ src/cmvp/views/BaseView.js
@@ -9,27 +9,39 @@ app.registerView(function (container) {
     var BaseView = {};
 
     BaseView.constructor = function (self, $scope, presenter, model) {
-        self.event = {};
-        self.fn = {};
-        self.data = {};
-        self.$scope = $scope;
-
-        $scope.event = self.event;
-        $scope.fn = self.fn;
-        $scope.data = self.data;
-
-        self.presenter = presenter;
-        self.model = model;
+        BaseView.constructorDI(self, {$scope: $scope, presenter: presenter, model: model});
     };
 
     BaseView.newInstance = function ($scope, $presenter, $model, classes) {
+        function constructor (View, di) {
+            return new View(di.$scope, di.presenter, di.model);
+        }
+        return BaseView.newInstanceDI({$scope: $scope, presenter: $presenter, model: $model}, classes, constructor);
+    };
+
+    BaseView.constructorDI = function (self, di) {
+        self.event = {};
+        self.fn = {};
+        self.data = {};
+        self.$scope = di.$scope;
+
+        self.$scope.event = self.event;
+        self.$scope.fn = self.fn;
+        self.$scope.data = self.data;
+
+        self.presenter = di.presenter;
+        self.model = di.model;
+    };
+
+    BaseView.newInstanceDI = function (di, classes, constructor) {
         var View = classes.view;
 
-        var scope = $scope || {};
-        var presenter = $presenter || makeNewInstanceOrThrow(classes.presenter, 'presenter');
-        var model = $model || makeNewInstanceOrThrow(classes.model, 'model');
+        di = di || {};
+        di.$scope = di.$scope || {};
+        di.presenter = di.presenter || makeNewInstanceOrThrow(classes.presenter, 'presenter');
+        di.model = di.model || makeNewInstanceOrThrow(classes.model, 'model');
 
-        var view = new View(scope, presenter, model);
+        var view = constructor ? constructor(View, di) : new View(di);
         ViewRepaintAspect.weave(view);
         return view;
     };
