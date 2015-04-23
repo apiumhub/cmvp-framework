c20b73ea35ba98e707c84a80beab82708bb9c2a5
Binh <binh.tran@apiumtech.com>
2015-04-23 11:49:16 +0700

    Moving assignmentId from $routeParams

--- test/helpers/Controller.js
+++ test/helpers/Controller.js
@@ -7,13 +7,13 @@ test.registerHelper("Controller", function() {
     var Scope = test.getHelper('Scope');
 
     return {
-        testCallsViewShow: function (Controller) {
+        testCallsViewShow: function (Controller, anotherParam) {
             describe("constructor", function () {
                 describe("always", function () {
                     it("should call the show method of the view", function () {
                         var $scope = Scope.getStub();
                         var spy = spyOn($scope, "$apply");
-                        var controller = new Controller($scope);
+                        var controller = new Controller($scope, anotherParam);
                         expect(spy).toHaveBeenCalled();
                     });
                 });
