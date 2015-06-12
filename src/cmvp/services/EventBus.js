f8a0aa250e8d83ec5ec6c9d9d41605682efcdc5a
Kien Nguyen <kien.nguyen@apiumtech.com>
2015-06-12 10:58:40 +0700

    Revert ID to eventBus

--- src/cmvp/services/EventBus.js
+++ src/cmvp/services/EventBus.js
@@ -5,15 +5,27 @@ app.register(function (container) {
     var postal = container.getObject('postal');
 
     function EventBus() {
-
+        this.subscriptions = {};
     }
 
-    EventBus.prototype.subscribe = function (parameters) {
-        return {
-            unsubscribe: postal.subscribe(parameters).unsubscribe,
+    EventBus.prototype.subscribe = function (parameters, id) {
+        var postalSubscription = postal.subscribe(parameters);
+        var newSubscription = {
+            unsubscribe: postalSubscription.unsubscribe.bind(postalSubscription),
             channel: parameters.channel,
             topic: parameters.topic
         };
+        if (id !== undefined) {
+            var path = [parameters.channel, parameters.topic, id].reduce(function (a, b) { return a + '/' + b });
+            if (this.subscriptions.hasOwnProperty(path)) {
+                var subscription = this.subscriptions[path];
+                subscription.unsubscribe();
+                // it's a singleton, it must be mutable
+                delete this.subscriptions[path];
+            }
+            this.subscriptions[path] = newSubscription;
+        }
+        return newSubscription;
     };
 
     EventBus.prototype.publish = function (parameters) {
