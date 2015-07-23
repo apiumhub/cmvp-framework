9aa6b86a383b4af7e041c285881402aed9c155b5
José manuel Barroso Galindo <theypsilon@gmail.com>
2015-07-23 20:19:51 +0700

    fixing issue

--- src/test-helpers/Presenter.js
+++ src/test-helpers/Presenter.js
@@ -27,55 +27,63 @@ define(function (require) {
                 });
             });
         },
-        exerciseRequestModel: function(options) {
-            var method = options.method,
-                modelMethod = options.modelMethod,
-                modelMethodCalledWith = options.modelMethodCalledWith,
-                successHandler = options.successHandler,
-                errorHandler = options.errorHandler,
-                viewMethod = options.viewMethod,
-                viewMethodReturn = options.viewMethodReturn;
+        exerciseRequestModel: function(getModel, getView, getSut) {
+            return function(options) {
+                var sut, model, view;
+                beforeEach(function() {
+                    sut = getSut();
+                    model = getModel();
+                    view = getView();
+                });
+                var method = options.method,
+                    modelMethod = options.modelMethod,
+                    modelMethodCalledWith = options.modelMethodCalledWith,
+                    successHandler = options.successHandler,
+                    errorHandler = options.errorHandler,
+                    viewMethod = options.viewMethod,
+                    viewMethodReturn = options.viewMethodReturn;
 
-            function exerciseCallHandler() {
-                if (viewMethod) view[viewMethod].returns(viewMethodReturn);
-                sut[method](view, model);
+                function exerciseCallHandler() {
+                    if (viewMethod) view[viewMethod].returns(viewMethodReturn);
+                    sut[method](view, model);
+                }
+
+                describe('always', function() {
+                    if (viewMethod) it('calls to view', function() {
+                        model[modelMethod].returns(PromiseHelper.fake('ok'));
+                        exerciseCallHandler();
+                        expect(view[viewMethod].called).toEqual(true);
+                    });
+
+                    it('calls to model', function() {
+                        model[modelMethod].returns(PromiseHelper.fake('ok'));
+                        exerciseCallHandler();
+                        expect(model[modelMethod].calledWith(modelMethodCalledWith)).toEqual(true);
+                    });
+                });
+
+                describe('when ' + method + ' is successful', function() {
+                    it('should call to view ' + successHandler, function() {
+                        model[modelMethod].returns(PromiseHelper.fake('ok'));
+                        exerciseCallHandler();
+                        expect(view[successHandler].called).toEqual(true);
+                    });
+                });
+
+                describe('when ' + method + ' is not successful', function() {
+                    it('should not call to view ' + successHandler, function() {
+                        model[modelMethod].returns(PromiseHelper.fake(undefined, 'error!'));
+                        exerciseCallHandler();
+                        expect(view[successHandler].called).toEqual(false);
+                    });
+
+                    it('should call to showError', function() {
+                        model[modelMethod].returns(PromiseHelper.fake(undefined, 'error!'));
+                        exerciseCallHandler();
+                        expect(view[errorHandler].called).toEqual(true);
+                    });
+                });
             }
-
-            describe('always', function() {
-                if (viewMethod) it('calls to view', function() {
-                    model[modelMethod].returns(PromiseHelper.fake('ok'));
-                    exerciseCallHandler();
-                    expect(view[viewMethod].called).toEqual(true);
-                });
-
-                it('calls to model', function() {
-                    model[modelMethod].returns(PromiseHelper.fake('ok'));
-                    exerciseCallHandler();
-                    expect(model[modelMethod].calledWith(modelMethodCalledWith)).toEqual(true);
-                });
-            });
-
-            describe('when ' + method + ' is successful', function() {
-                it('should call to view ' + successHandler, function() {
-                    model[modelMethod].returns(PromiseHelper.fake('ok'));
-                    exerciseCallHandler();
-                    expect(view[successHandler].called).toEqual(true);
-                });
-            });
-
-            describe('when ' + method + ' is not successful', function() {
-                it('should not call to view ' + successHandler, function() {
-                    model[modelMethod].returns(PromiseHelper.fake(undefined, 'error!'));
-                    exerciseCallHandler();
-                    expect(view[successHandler].called).toEqual(false);
-                });
-
-                it('should call to showError', function() {
-                    model[modelMethod].returns(PromiseHelper.fake(undefined, 'error!'));
-                    exerciseCallHandler();
-                    expect(view[errorHandler].called).toEqual(true);
-                });
-            });
         }
     };
 });
